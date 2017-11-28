4a66a760b36569056e8366761658495c8f7d345e
Zana Aziz <zanaadaniel@gmail.com>
2017-11-28 17:59:57 +0000

    created basic skeleton of app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weather4U' });
+  res.render('index', {
+      title: 'Weather4U'
+      
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+      title: 'Weather4U'
+      
+  });
+});
+
+/* GET locations page. */
+router.get('/locations', function(req, res, next) {
+  res.render('locations', {
+      title: 'Weather4U'
+      
+  });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+      title: 'Weather4U'
+      
+  });
 });
 
 module.exports = router;
