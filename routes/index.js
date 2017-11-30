bc98b96a045dccdb8e256ad73f13fb7e15c12a60
Zana Aziz <zanaadaniel@gmail.com>
2017-11-30 13:21:26 +0000

    weather search nearly implemented

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var YQL = require('yql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'Weather4U'
-      
+    title: 'Weather4U'
+  });
+});
+
+/* POST weather search */
+router.post('/', function (req, res) {
+  res.render('index', {
+    title: 'Weather4U'
+  });
+  
+  var query = new YQL(`select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${req.body.location}") and u="c" `);
+  
+  query.exec(function(err, data) {
+    var location = data.query.results.channel.location;
+    var condition = data.query.results.channel.item.condition;
+    var weather = data.query.results.channel.item.forecast;
+    
+    console.log("\n\n");
+    console.log('The current weather in ' + location.city + ', ' + location.country + ' is ' + condition.text + ' at ' + condition.temp + ' degrees.');
+    console.log("\n\n");
+    console.log(weather);
+    console.log("\n\n");
   });
 });
 
